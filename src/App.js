c45570cd1fad9499ac69b7da1fcbab65d1854ed9
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2021-10-16 05:57:57 +0300

    edit about text style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import './App.scss';
-import About from './Container/About/About';
 import Contact from './Components/Contact/Contact';
 import DownloadButton from './Components/DownloadButton/DownloadButton';
+import Home from './Container/Home/Home';
 
 const App = () => {
   return (
     <div className="app">
       <DownloadButton />
       <Contact />
-      <About /> 
+      <Home /> 
     </div>
   );
 }
---
+++ src/Container/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+import About from '../About/About';
+import './Home.scss';
+
+const Home = () => {
+  return (
+      <div className="home">
+          <About />
+      </div>
+  )
+};
+
+export default Home;
